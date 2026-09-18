75e97c05e7a75617ab57398ca338d484ddc73bdc
Viachaslau Kazakou <sly.kazakoff@gmail.com>
2026-09-18 11:48:30 +0200

    Revert "Load project config from working directory"
    This reverts commit 9e559d76cd8b715a11d36960c3855967dea9a427.

--- src/tools.rs
+++ src/tools.rs
@@ -565,12 +565,6 @@ impl ToolRegistry {
             .collect()
     }
 
-    pub fn names(&self) -> Vec<String> {
-        let mut names = self.tools.keys().cloned().collect::<Vec<_>>();
-        names.sort();
-        names
-    }
-
     pub async fn execute(
         &self,
         name: &str,
